3b8d80865099a4c4907a6c33878c98175bc36073
Your Name <mattpearson94@gmail.com>
2023-03-16 16:04:49 +0000

    added the listening events

--- script.js
+++ script.js
@@ -4,8 +4,16 @@ const container = document.querySelector('.container')
 const cells = document.createElement('div')
 const info = document.querySelector('.instruction')
 
+// 1 is cross
+// 0 is naught
+
+let turn = 1
 let go = 'cross'
 let instructions = 'Cross\'s turn'
+let crossChoices = []
+let naughtChoices = []
+const winningCombinations = [[0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 4, 8], [2, 4, 6]]
+
 
 const boardCells = [ "", "", "", 
                     "", "", "", 
@@ -32,16 +40,47 @@ document.querySelectorAll('.cells').forEach((item, index) => {
         go = (go === 'cross') ? 'circle' : 'cross'
         instructions = (instructions === 'Cross\'s turn') ? 'Naught\'s turn' : 'Cross\'s turn'
         info.textContent = instructions
+        if (go === 'cross') {
+            naughtChoices.push(index)
+        } else {
+            crossChoices.push(index)
+        }
         item.removeEventListener('click', event)
+        arraysMatch(crossChoices)
+
     })
 })
 
+function arraysMatch(arr1) {
+    	if (arr1.length !== 3) return false;
+    	for (var i = 0; i < arr1.length; i++) {
+            arr1.sort()
+    		if (arr1[i] !== winningCombinations[0][i]) return false;
+    	}
+    
+    	// Otherwise, return true
+    	console.log('winner');
+    
+    };
 
-// cell0.addEventListener("click", choice)
 
-// function choice() {
-//     const selection = document.createElement('div')
-//     selection.classList.add(`${go}`)
-//     cell0.appendChild(selection)
-//     cell0.removeEventListener("click", choice)
-// }
+// function arraysMatch(arr1, arr2) {
+// 	if (arr1.length !== arr2.length) return false;
+// 	for (var i = 0; i < arr1.length; i++) {
+//         arr2.sort()
+// 		if (arr1[i] !== arr2[i]) return false;
+// 	}
+
+// 	// Otherwise, return true
+// 	console.log('winner');
+
+// };
+
+let array1 = [1,2,3],
+    array2 = [1,2,3,4],
+    array3 = [1,2];
+
+let checker = (arr, target) => target.every(v => arr.includes(v));
+
+console.log(checker(array2, array1));  // true
+console.log(checker(array3, array1));  // false
